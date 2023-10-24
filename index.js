1a7d91f5fb8f98ad230e703547453fd7cb98ef4e
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-24 23:15:44 +0200

    Added mode controls + completed UI

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let gridSize = 16;
+    let gridSize = 64;
+    let colorMode = "classic";
+    let isDrawing = false;
 
+    // Constants
+    const classicColor = "#707070";
+    const randomPalette = ['#EF476F', '#FF5733', '#FFD166', '#33FF57', '#06D6A0', '#118AB2', '#6200EA'];
+
+    // DOM Elements
     const gridContainer = document.querySelector("#grid-container");
     const toyContainer = document.querySelector(".toy-container");
-    const eraseButton = document.querySelector("#erase-btn");
     const sizeControls = document.querySelector(".size-controls");
+    const modeControls = document.querySelector(".mode-controls");
+    const toyControls = document.querySelector(".toy-controls");
 
     generateGrid(gridSize);
 
     // Event Listeners
+    gridContainer.addEventListener("mousedown", (event) => {
+        isDrawing = true;
+    });
+
+    gridContainer.addEventListener("mouseup", (event) => {
+        isDrawing = false;
+    });
+
     gridContainer.addEventListener("mouseover", function (event) {
+        if (!isDrawing) return;
         if (event.target.classList.contains("grid-item")) {
-            event.target.style.backgroundColor = "gray";
+            if (colorMode === 'classic') {
+                event.target.style.backgroundColor = classicColor;
+            } else if (colorMode === 'random') {
+                const randomColor = Math.floor(Math.random() * randomPalette.length);
+                event.target.style.backgroundColor = randomPalette[randomColor];
+            }
+        };
+    });
+
+    toyContainer.addEventListener('click', (event) => {
+        if (event.target === toyContainer) {
+            erase();
+            shakeToy();
         }
     });
 
-    eraseButton.addEventListener('click', () => {
-        erase();
-        shakeToy();
+    toyControls.addEventListener('click', (event) => {
+        event.stopPropagation();
     });
+
     sizeControls.addEventListener('click', (event) => {
         if (event.target.value) {
             handleSizeSelection(event.target.value)
         }
     });
+
+    modeControls.addEventListener('click', (event) => {
+        if (event.target.value) {
+            handleModeChange(event.target.value);
+        }
+    })
 
     // Main Logic
     function generateGrid(size) {
@@ -46,6 +81,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function handleSizeSelection(newGridSize) {
         erase();
         generateGrid(newGridSize);
+    }
+
+    function handleModeChange(mode) {
+        colorMode = mode;
     }
 
     function erase() {
