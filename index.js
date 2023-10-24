37e9aa45f76c6ca95253adb5adf3f3a91b7bb5a6
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-24 16:15:38 +0200

    Grid generation function complete

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // DOM elements
+    const [gridContainer] = ['#grid_container'].map(selector => document.querySelector(selector));
+
+    generateGrid(16, 16);
+
+    function generateGrid(cols, rows) {
+        gridContainer.style.setProperty('--grid-rows', rows);
+        gridContainer.style.setProperty('--grid-cols', cols);
+        for (let c = 0; c < (rows * cols); c++) {
+            let cell = document.createElement('div');
+            cell.classList.add('grid-item');
+            gridContainer.appendChild(cell);
+        }
+    }
 })
