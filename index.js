c1d10ff2a96377a4f8d624786388de81f65f1414
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-24 18:36:36 +0200

    Improved UI + added erase button

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    let currentGridSize;
     generateGrid(16);
 
+    // Event Listeners
     document.getElementById("grid-container").addEventListener("mouseover", function (event) {
         if (event.target.classList.contains("grid-item")) {
             event.target.style.backgroundColor = "gray";
         }
     });
-})
 
-function generateGrid(size) {
-    resetGrid();
+    document.getElementById("erase-btn").addEventListener('click', () => generateGrid(currentGridSize));
 
-    const gridContainer = document.getElementById("grid-container");
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    function generateGrid(size) {
+        resetGrid();
 
-    for (let i = 0; i < size * size; i++) {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
-        gridContainer.appendChild(gridItem);
+        currentGridSize = size;
+        const gridContainer = document.getElementById("grid-container");
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+        for (let i = 0; i < size * size; i++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridContainer.appendChild(gridItem);
+        }
     }
-}
 
-function resetGrid() {
-    const gridElements = document.querySelectorAll('.grid-item');
-    gridElements.forEach(element => element.remove());
-}
+    function resetGrid() {
+        const gridElements = document.querySelectorAll('.grid-item');
+        gridElements.forEach(element => element.remove());
+    }
+})
