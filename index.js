83e969cb003af24a797fe06aadd6024d2c051a47
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-24 19:37:55 +0200

    Improved UI

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let gridSize = 16;
+    const gridContainer = document.querySelector("#grid-container");
+    const toyContainer = document.querySelector(".toy-container");
+    const eraseButton = document.querySelector("#erase-btn");
 
-    let currentGridSize;
-    generateGrid(16);
+    generateGrid(gridSize);
 
-    // DOM Elements
-    const gridContainer = document.querySelector('#grid-container');
-    const toyContainer = document.querySelector('.toy-container');
-    const eraseButton = document.querySelector('#erase-btn');
-
-    // Event Listeners
     gridContainer.addEventListener("mouseover", function (event) {
         if (event.target.classList.contains("grid-item")) {
             event.target.style.backgroundColor = "gray";
         }
-
     });
 
-    eraseButton.addEventListener('click', () => {
-        const gridElements = document.querySelectorAll('.grid-item');
-        gridElements.forEach(element => {
-            element.style.backgroundColor = "#fff";
-            element.classList.add("erasing");
-        });
-
-        toyContainer.classList.add("shaking");
-        setTimeout(() => {
-            gridElements.forEach(element => element.classList.remove("erasing"));
-            toyContainer.classList.remove("shaking");
-        }, 500);
-    });
+    eraseButton.addEventListener('click', erase);
 
     function generateGrid(size) {
         resetGrid();
 
-        currentGridSize = size;
-        const gridContainer = document.getElementById("grid-container");
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
         for (let i = 0; i < size * size; i++) {
@@ -48,4 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const gridElements = document.querySelectorAll('.grid-item');
         gridElements.forEach(element => element.remove());
     }
-})
+
+    function erase() {
+        const gridElements = document.querySelectorAll('.grid-item');
+        gridElements.forEach(element => {
+            element.classList.add("erasing");
+            element.style.backgroundColor = "#fff";
+            setTimeout(() => {
+                element.classList.remove("erasing");
+            }, 500);
+        });
+        shakeToy();
+    }
+
+    function shakeToy() {
+        toyContainer.classList.add("shaking");
+        setTimeout(() => {
+            toyContainer.classList.remove("shaking");
+        }, 500);
+    }
+});
